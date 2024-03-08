238b8c2de8e948de962c83d9d5ee1afba2f46035
Jelly <jelly@befuture8.com>
2024-03-08 16:24:30 +0800

    feat: add new props for customized

--- src/champion.vue
+++ src/champion.vue
@@ -1,32 +1,35 @@
 <script setup>
-import { ref, onMounted, watch } from 'vue';
-import { formatChampionData } from './format';
-import { drawTable } from './canvas';
+import { ref, onMounted, watch } from "vue";
+import { formatChampionData } from "./format";
+import { drawTable } from "./canvas";
 
 const props = defineProps({
-    data: Array,
-    type: Number
+  data: Array,
+  type: Number,
+  issueTitle: String,
+  trendTitle: Array,
+  rankingNumber: Number,
 });
 
 const canvasRef = ref(null);
 
 onMounted(() => {
-    const newData = formatChampionData(props.data);
-    const canvas = canvasRef.value;
-    drawTable(canvas, newData, props.type);
+  const newData = formatChampionData(props.data, props.rankingNumber);
+  const canvas = canvasRef.value;
+  drawTable(canvas, newData, props.type, props.issueTitle, props.trendTitle);
 });
 
 watch(
-    () => props.data,
-    () => {
-        const newData = formatChampionData(props.data);
-        const canvas = canvasRef.value;
-        drawTable(canvas, newData, props.type);
-    },
-    { deep: true }
+  () => props.data,
+  () => {
+    const newData = formatChampionData(props.data, props.rankingNumber);
+    const canvas = canvasRef.value;
+    drawTable(canvas, newData, props.type, props.issueTitle, props.trendTitle);
+  },
+  { deep: true }
 );
 </script>
 
 <template>
-    <canvas ref="canvasRef" width="1240"></canvas>
+  <canvas ref="canvasRef" width="1240"></canvas>
 </template>
