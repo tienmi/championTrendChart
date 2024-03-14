e0db3e9ff6c7d155a5fe19d499fc298ef425b37c
Jelly <jelly@befuture8.com>
2024-03-14 18:54:16 +0800

    feat: set prettier to 4 tabWidth

--- src/champion.vue
+++ src/champion.vue
@@ -4,32 +4,38 @@ import { formatChampionData } from "./format";
 import { drawTable } from "./canvas";
 
 const props = defineProps({
-  data: Array,
-  type: Number,
-  issueTitle: String,
-  trendTitle: Array,
-  rankingNumber: Number,
+    data: Array,
+    type: Number,
+    issueTitle: String,
+    trendTitle: Array,
+    rankingNumber: Number,
 });
 
 const canvasRef = ref(null);
 
 onMounted(() => {
-  const newData = formatChampionData(props.data, props.rankingNumber);
-  const canvas = canvasRef.value;
-  drawTable(canvas, newData, props.type, props.issueTitle, props.trendTitle);
-});
-
-watch(
-  () => props.data,
-  () => {
     const newData = formatChampionData(props.data, props.rankingNumber);
     const canvas = canvasRef.value;
     drawTable(canvas, newData, props.type, props.issueTitle, props.trendTitle);
-  },
-  { deep: true }
+});
+
+watch(
+    () => props.data,
+    () => {
+        const newData = formatChampionData(props.data, props.rankingNumber);
+        const canvas = canvasRef.value;
+        drawTable(
+            canvas,
+            newData,
+            props.type,
+            props.issueTitle,
+            props.trendTitle
+        );
+    },
+    { deep: true }
 );
 </script>
 
 <template>
-  <canvas ref="canvasRef" width="1240"></canvas>
+    <canvas ref="canvasRef" width="1240"></canvas>
 </template>
